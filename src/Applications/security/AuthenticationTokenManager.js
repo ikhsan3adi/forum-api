9c5fdea23ef620844281fc60fcd9d8353a807859
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-10-29 10:53:06 +0700

    Update AuthenticationTokenManager.js

--- src/Applications/security/AuthenticationTokenManager.js
+++ src/Applications/security/AuthenticationTokenManager.js
@@ -12,7 +12,7 @@ class AuthenticationTokenManager {
     throw new Error('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
   }
 
-  async decodePayload() {
+  async decodePayload(token) {
     throw new Error('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
   }
 }
