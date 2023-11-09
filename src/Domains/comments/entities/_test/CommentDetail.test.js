c6dfc9bcefa7c1aef673c51606755c06da830905
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-11-09 09:39:43 +0700

    add test case for deleted comment & reply

--- src/Domains/comments/entities/_test/CommentDetail.test.js
+++ src/Domains/comments/entities/_test/CommentDetail.test.js
@@ -56,4 +56,35 @@ describe('CommentDetail entities', () => {
     expect(commentDetail.replies).toEqual(payload.replies);
     expect(commentDetail.date).toEqual(payload.date);
   });
+
+  it('should create deleted CommentDetail entities correctly', () => {
+    // Arrange
+    const payload = {
+      id: 'comment-123',
+      username: 'foobar',
+      content: 'a comment',
+      replies: [
+        {
+          id: 'replies-1',
+          username: 'johndoe',
+          content: 'a reply',
+          date: '2023-09-21T23:59:59.555Z',
+        },
+      ],
+      date: '2023-09-22T07:19:09.775Z',
+      likeCount: 0,
+      is_delete: true,
+    };
+
+    // Action
+    const commentDetail = new CommentDetail(payload);
+
+    // Assert
+    expect(commentDetail).toBeInstanceOf(CommentDetail);
+    expect(commentDetail.id).toEqual(payload.id);
+    expect(commentDetail.username).toEqual(payload.username);
+    expect(commentDetail.content).toEqual('**komentar telah dihapus**');
+    expect(commentDetail.replies).toEqual(payload.replies);
+    expect(commentDetail.date).toEqual(payload.date);
+  });
 });
